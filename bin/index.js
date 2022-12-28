b110e45afce07bbdd694991b5a6761070d4f8163
alexisben <alex@noesya.coop>
2022-12-28 12:10:43 +0100

    cleanup

--- bin/index.js
+++ bin/index.js
@@ -4,39 +4,6 @@ const yargs = require("yargs");
 const { exec } = require("child_process");
 
 let updateCommand = `git submodule foreach git pull || echo 'canceled' && git commit -am 'theme' && git pull ; git push`;
-const sequence = [
-  'cd themes/osuny-hugo-theme-aaa',
-  'git checkout main',
-  'git pull',
-  'cd ../..',
-  'git add .',
-  'git commit -m "theme"',
-  // 'git push'
-]
-
-function chain(commands) {
-
-  exec(commands[0], (error, stdout, stderr) => {
-    if (error) {
-      console.error(`error: ${error.message}`);
-      return;
-    }
-
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return;
-    }
-
-    if (stdout) {
-      console.log(`stdout:\n${stdout}`);
-    }
-
-    commands.shift()
-    if (commands.length > 0) {
-      chain(commands);
-    }
-  });
-}
 
 const options = yargs
   .usage('$0 <cmd> [args]')
