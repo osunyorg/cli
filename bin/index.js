d6d627fd211be0a8d523509a2632aab1234fc42d
alexisben <alex@noesya.coop>
2022-12-28 12:12:43 +0100

    add push in options

--- bin/index.js
+++ bin/index.js
@@ -3,19 +3,27 @@
 const yargs = require("yargs");
 const { exec } = require("child_process");
 
-let updateCommand = `git submodule foreach git pull || echo 'canceled' && git commit -am 'theme' && git pull ; git push`;
+let updateCommand = `git submodule foreach git pull || echo 'canceled' && git commit -am 'theme' && git pull`;
 
 const options = yargs
   .usage('$0 <cmd> [args]')
-  .command('update [path]', "update website's theme", (yargs) => {
+  .command('update [path] [push]', "update website's theme", (yargs) => {
     yargs.positional('path', {
       type: 'string',
       describe: 'path to website'
+    })
+    yargs.positional('push', {
+      type: 'boolean',
+      describe: 'push commit'
     })
   }, (argv) => {
 
     if (argv.path) {
       updateCommand = `cd ${argv.path} && ${updateCommand}`
+    }
+
+    if (argv.push) {
+      updateCommand = `; git push`
     }
 
     exec(updateCommand, (error, stdout, stderr) => {
