0955bb4664623dbbed75c5927f703b7f25273d55
alexisben <alex@noesya.coop>
2023-11-13 18:35:34 +0100

    add command to expose serve on LAN

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 
 const shell = require("shelljs");
-
 const repositories = require("../data/repositories");
+const os = require('os');
+
 
 // commands
 const commands = {
   "clone-all": function(argv) {
     const path = argv[3] || '.';
 
-    shell.cd(`${path}`);
+    shell.cd(path);
     repositories.forEach(repository => {
       console.log(`osuny cloning ${repository}`);
       shell.exec(`git clone --recurse-submodules ${repository}`);
@@ -20,14 +21,43 @@ const commands = {
 
     shell.set('-e'); // exit upon first error
 
-    shell.cd(`${path}`);
+    shell.cd(path);
 
     shell.ls('.').forEach(function (folder) {
-      console.log(folder)
       shell.cd(folder)
       shell.exec('git pull origin main --recurse-submodules \;')
       shell.cd('..')
     });
+  },
+  "update": function(argv) {
+    const path = argv[3];
+    shell.set('-e'); // exit upon first error
+
+    if (!path) {
+      return console.log('need path');
+    }
+
+    shell.cd(path)
+    shell.exec('git pull origin main --recurse-submodules \;')
+  },
+  "serve": function(argv) {
+    const networkInterfaces = os.networkInterfaces();
+    let localIP = null;
+
+    for(let entry in networkInterfaces) {
+      networkInterfaces[entry].forEach((config) => {
+        if (config.address.includes("192.168")) {
+          localIP = config.address;
+        }
+      })
+    }
+
+    if (localIP) {
+      shell.exec(`hugo serve --bind ${localIP} --baseUrl http://${localIP} -p 8000`);
+      console.log(`running local network on : http://${localIP}`)
+    } else {
+      shell.exec('hugo serve');
+    }
   }
 }
 
